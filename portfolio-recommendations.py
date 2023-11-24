a14ceabb137eb749cfa71e8842e3e91efa9a9c0d
system-not-responding <51693328+system-not-responding@users.noreply.github.com>
2023-11-24 09:41:42 -0700

    small tweaks

--- portfolio-recommendations.py
+++ portfolio-recommendations.py
@@ -1,14 +1,12 @@
-portfolio_balance = input("\nWhat's your portfolio balance? $")
+portfolio_balance = input("\nWhat's your portfolio balance?\n>>> $")
 portfolio_balance = float(portfolio_balance)
-print(f"Your portfolio's current balance is: ${portfolio_balance}")
 
 recommended_percent = .3
 cash_percent = portfolio_balance * recommended_percent
 cash_percent = portfolio_balance - cash_percent
 cash_percent = round(float(cash_percent), 2)
-print(f"The current recommended cash balance is: ${cash_percent}")
+print(f"\nRecommended cash balance is: ${cash_percent}")
 
 risk_percent = portfolio_balance - cash_percent
-print(f"Your recommened risk balance is: ${risk_percent}\n")
-
+print(f"Recommened risk balance is: ${risk_percent}\n")
 
