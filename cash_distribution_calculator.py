666c017ad5ef95410062c58f0668212c20c06e70
system-not-responding <justino.alexander14@gmail.com>
2024-01-04 09:57:55 -0700

    Rename file and update cash distribution calculation
    The file 'portfolio-recommendations.py' has been renamed to 'cash_distribution_calculator.py'. Moreover, the calculation of the recommended cash percent has been updated to account for current three-month-rate and average benchmark return. This change makes the calculation more dynamic and suitable for changing market conditions.

--- cash_distribution_calculator.py
+++ cash_distribution_calculator.py
@@ -1,12 +1,18 @@
 portfolio_balance = input("\nWhat's your portfolio balance?\n>>> $")
 portfolio_balance = float(portfolio_balance)
 
-recommended_percent = .3
+
+three_month_rate = float(input("What's the current three-month-rate?\n>>> "))
+avg_benchmark_return = 8
+
+recommended_percent = (avg_benchmark_return - three_month_rate) / 10
+
 cash_percent = portfolio_balance * recommended_percent
 cash_percent = portfolio_balance - cash_percent
 cash_percent = round(float(cash_percent), 2)
 print(f"\nRecommended cash balance is: ${cash_percent}")
 
 risk_percent = portfolio_balance - cash_percent
-print(f"Recommened risk balance is: ${risk_percent}\n")
+print(f"Recommended risk balance is: ${risk_percent}\n")
+
 
