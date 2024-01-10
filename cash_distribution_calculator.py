f4a1c0d78972128c530266596cb0382542215173
justinoo <justino.alexander14@gmail.com>
2024-01-10 16:12:30 -0700

    Update .gitignore, misc.xml and refactor cash_distribution_calculator.py
    Added .gitignore and misc.xml files in the .idea directory for project setup. In the cash_distribution_calculator.py file, the input process is simplified for portfolio balance and three-month-rate. Also, variable names in the calculation and printing of cash and risk balances have been improved for better readability and understanding.

--- cash_distribution_calculator.py
+++ cash_distribution_calculator.py
@@ -1,18 +1,15 @@
-portfolio_balance = input("\nWhat's your portfolio balance?\n>>> $")
-portfolio_balance = float(portfolio_balance)
+print("\n")
+portfolio_balance = float(input("Enter your portfolio balance: $"))
 
-
-three_month_rate = float(input("What's the current three-month-rate?\n>>> "))
+three_month_rate = float(input("Enter the current three-month-rate: "))
 avg_benchmark_return = 8
 
-recommended_percent = (avg_benchmark_return - three_month_rate) / 10
+ratio = (avg_benchmark_return - three_month_rate) / 10
 
-cash_percent = portfolio_balance * recommended_percent
-cash_percent = portfolio_balance - cash_percent
-cash_percent = round(float(cash_percent), 2)
-print(f"\nRecommended cash balance is: ${cash_percent}")
+cash_balance = portfolio_balance * (1 - ratio)
+cash_balance = round(cash_balance, 2)
 
-risk_percent = portfolio_balance - cash_percent
-print(f"Recommended risk balance is: ${risk_percent}\n")
+risk_balance = portfolio_balance - cash_balance
 
-
+print(f"\nRecommended cash balance: ${cash_balance}")
+print(f"Recommended risk balance: ${risk_balance}\n")
